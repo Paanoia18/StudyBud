35879347bf94de1576ec9d051cc473fc4bec1b99
Paanoia18 <149941959+Paanoia18@users.noreply.github.com>
2024-06-14 13:33:36 +0300

    gara1

--- base/api/urls.py
+++ base/api/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',  views.getRoutes),
+path('api/rooms/<int:id>/', views.delete_room, name='delete_room'),
     path('rooms/', views.getRooms),
     path('rooms/<str:pk>/', views.getRoom),
 ]
--- base/api/views.py
+++ base/api/views.py
@@ -1,3 +1,5 @@
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from base.models import Room
@@ -14,6 +16,16 @@ def getRoutes(request):
     ]
     return Response(routes)
 
+@csrf_exempt
+def delete_room(request, id):
+    if request.method == 'DELETE':
+        try:
+            room = Room.objects.get(id=id)
+            room.delete()
+            return JsonResponse({'message': 'Комната удалена'}, status=200)
+        except Room.DoesNotExist:
+            return JsonResponse({'message': 'Комната не найдена'}, status=404)
+    return JsonResponse({'message': 'Неверный метод'}, status=405)
 
 @api_view(['GET'])
 def getRooms(request):
